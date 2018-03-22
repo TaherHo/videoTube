34f50c5b5425c52a6911ef4a3124afb68816e9b1
Taher Hosseini <hosseini@publicplan.de>
2018-03-22 15:19:03 +0100

    some component gepusht

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import Search from '../components/Search';
+export default class App extends Component {
+    render () {
+        return( <div>
+          <p>This is my new react app</p>
+          <Search/>
+        </div>)
+    }
+}
--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,30 +1,36 @@
 
-var webpack = require('webpack');
-var path = require('path');
+let webpack = require('webpack');
+const  path = require('path');
 
-var parentDir = path.join(__dirname, '../');
+let parentDir = path.join(__dirname, '../');
 
 module.exports = {
     entry: [
         path.join(parentDir, 'index.js')
     ],
     module: {
-        loaders: [{
+        loaders: [
+          {
             test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
-            },{
+            },
+          {
                 test: /\.less$/,
                 loaders: ["style-loader", "css-loder", "less-loader"]
+            },
+          {
+            test: /\.scss$/,
+              loader: ["style-loader", "css-loader", "sass-loader"]
             }
         ]
     },
     output: {
-        path: parentDir + '/dist',
+        path: path.join( parentDir , '/dist'),
         filename: 'bundle.js'
     },
     devServer: {
         contentBase: parentDir,
         historyApiFallback: true
     }
-}
+};
